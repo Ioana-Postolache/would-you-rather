f453089dee359957b831f2b311286e811c649470
Ioana-Postolache <ioana.postolache.zaharia@gmail.com>
2019-01-08 18:07:08 +0000

    initial-setup-utils

---
+++ src/utils/helpers.js
@@ -0,0 +1,11 @@
+import { formatQuestion } from './_DATA'
+export function formatDate (timestamp) {
+  const d = new Date(timestamp)
+  const time = d.toLocaleTimeString('en-US')
+  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+}
+
+export function formatQuestion (info){
+  return formatQuestion(info)
+}
+
